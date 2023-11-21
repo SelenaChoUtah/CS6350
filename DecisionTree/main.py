79f5e5cbe4af1fe299148b6e4a82e950611d8540
SelenaChoUtah <121050135+SelenaChoUtah@users.noreply.github.com>
2023-11-21 10:59:15 -0700

    Update main.py

--- DecisionTree/main.py
+++ DecisionTree/main.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 import datetime
-from Perceptron import *
+from svm import *
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
 
 if __name__ == "__main__": 
     print('Start Time: ',datetime.datetime.now())
@@ -17,42 +20,64 @@ if __name__ == "__main__":
     X_test = test_data.iloc[:, :-1].values
     y_test = test_data.iloc[:, -1].values
 
-    # Augment for bias term
-    X_train = np.column_stack((X_train, np.ones(X_train.shape[0])))
-    X_test = np.column_stack((X_test, np.ones(X_test.shape[0])))
-
+    # Preprocess Data
+    y_train = np.where(y_train == 0, -1, 1)  # Convert labels to {1, -1}
+    y_test = np.where(y_test == 0, -1, 1)
     # print(X_train)
 
-    # Make sure the labels are in the format of -1 (for forged) and 1 (for genuine)
-    # Output is a label y ∈ {-1, 1}
-    y_train = np.where(y_train == 0, -1, 1)
-    y_test = np.where(y_test == 0, -1, 1)
+    C_values = [100 / 873, 500 / 873, 700 / 873]
+    gamma_0_values = [0.1,0.01, 0.001]  # tune for convergence
+    a_values = [1, 0.1, 0.01]  # tune for convergence
+    T = 100  # Maximum epochs
 
-    # Standard Perceptron
-    learned_weights, learned_bias, training_errors = perceptron_train(X_train, y_train, max_epochs=10)
+    # Problem 2
+    tra, tea, wa, ba = schedule2A(C_values, gamma_0_values, a_values, T, X_train, y_train, X_test, y_test)
+    trb, teb, wb, bb = schedule2B(C_values, gamma_0_values, a_values, T, X_train, y_train, X_test, y_test)
 
-    # Test the Perceptron on the test dataset
-    test_error = perceptron_test(X_test, y_test, learned_weights, learned_bias)
+    print(np.array(tra)-np.array(trb))
+    print(np.array(tea)-np.array(teb))
+    print(np.array(ba) - np.array(bb))
 
-    print("Standard Perceptron: Learned Weight Vector:", learned_weights)
-    print("Standard Perceptron: Average Prediction Error on Test Dataset:", test_error)
-    print("Standard Perceptron: Average Prediction Accuracy on Test Dataset:", 1-test_error)
+    # Problem 3A: Dual SVM Learning
+    dual_svm(C_values, X_train, y_train)
 
-    # Voted Perceptron
-    weights_list, counts = voted_perceptron_train(X_train, y_train, max_epochs=10)
+    # Problem 3B: Gaussian Kernal in Dual SVM Learning
+    # Test different values of gamma and C    
+    gamma_values = [0.1, 0.5, 1, 5, 100]
+    C_values = [100 / 873, 500 / 873, 700 / 873]
 
-    # Test the Voted Perceptron on the test dataset
-    test_error = voted_perceptron_test(X_test, y_test, weights_list, counts)
+    gaussian_svm(gamma_values, C_values, X_train, X_test, y_train, y_test)
 
-    # Print the list of distinct weight vectors and their counts
-    for i, (w, count) in enumerate(zip(weights_list, counts)):
-        print(f"Weight Vector {i + 1}: {np.round(w,3)}, Count: {count}")
+    # Problem 3C: Report number of overlapped support vectors
+    # support_vectors_per_gamma = {}  # Dictionary to store support vectors for each gamma
+    C_value = 500 / 873
+    support_vectors_per_gamma = {}  # Dictionary to store support vectors for each gamma
 
-    print("Voted Perceptron: Average Test Error:", test_error)
-    print("Voted Perceptron: Average Accuracy Error:", 1-test_error)
+    for gamma in gamma_values:
+        for C in C_values:
+            # Train SVM with Gaussian Kernel
+            alpha = fit(X_train, y_train, C, gamma)
+            
+            # Find support vectors
+            sv_indices = np.where(alpha > 1e-5)[0]
+            support_vectors = X_train[sv_indices]
+            support_vectors_per_gamma[gamma] = support_vectors
+            print(f"For gamma={gamma}, C={C}: Number of support vectors = {len(support_vectors)}")
 
-    # Average Perceptron
-    avg_weight = average_perceptron_train(X_train, y_train, max_epochs=10)
-    avg_error = average_perceptron_test(X_test, y_test, avg_weight)
-    print("Average Perceptron: Weight Vector: ",avg_weight)
-    print("Average Perceptron: Average Prediction Error: ", avg_error)
+    # Find overlapped support vectors between consecutive gamma values
+    overlapped_support_vectors = {}
+    prev_sv = None
+
+    for gamma, sv in support_vectors_per_gamma.items():
+        if prev_sv is not None:
+            overlapped = np.intersect1d(prev_sv, sv, assume_unique=True)
+            overlapped_support_vectors[(prev_gamma, gamma)] = len(overlapped)
+        
+        prev_gamma = gamma
+        prev_sv = sv
+        
+    # Report the number of overlapped support vectors
+    for gammas, count in overlapped_support_vectors.items():
+        print(f"Overlapped support vectors between gamma={gammas[0]} and gamma={gammas[1]}: {int(np.round(count/5))}")
+
+   
